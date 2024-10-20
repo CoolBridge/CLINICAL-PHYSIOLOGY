3cdd59637056afd58fa1f6589a2a2ff00387796a
CoolBridge <47200920+CoolBridge@users.noreply.github.com>
2024-10-20 13:34:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.details-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('More information about the electrocardiography program will be displayed here.');
+        // Here you can implement modal opening or redirecting to a detailed page
+    });
+});
